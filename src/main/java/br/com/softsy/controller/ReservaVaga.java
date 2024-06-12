559e72d10f7ebe797f2c4b37c345624cb2f02d89
luyz-softsy <luiz.silva@softsy.io>
2024-06-12 15:13:33 -0300

    controller de reserva vaga criada

---
+++ src/main/java/br/com/softsy/controller/ReservaVaga.java
@@ -0,0 +1,21 @@
+package br.com.softsy.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//import br.com.softsy.model.UsuarioInternoVO;
+//import br.com.softsy.utils.LoginUtils;
+
+@Controller
+public class ReservaVaga {
+	
+	@RequestMapping(value = { "dadosResponsavel" }, method = RequestMethod.GET)
+	public String login(HttpSession session, Model model) throws Exception {
+ 
+		return "reservaVaga/dadosResponsavel";
+	}
+}
